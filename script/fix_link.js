1c4ee846f1470040658bccac050793d7fa5b54c5
SLboat <slboat@me.com>
2013-06-14 21:29:13 +0800

    构建基础的连接跳转

--- script/fix_link.js
+++ script/fix_link.js
@@ -1,41 +1,19 @@
 
-function checkForValidUrl(tabId, changeInfo, tab) {
-	//todo:处理损坏的浏览器地址
-	//console.log("当前地址:",tab.url)
+function chkfixurl(tabId, changeInfo, tab) {
 	
 	//跳转地址
 	if (tab.url=="chrome-extension://bfpafjgbeaafloinkjifblnjgelpkgpf/?q=%E5%89%A5%E7%BA%BF%E9%92%B3"){
 		chrome.tabs.update(tabId,{url: "http://www.baidu.com"})
-
+		//显示图标
+		show_icon(tabID);
 	}
 
-	if (tab.url.indexOf('see.sl088.com') > -1) {
-		//显示它
-	  	chrome.pageAction.show(tabId);
-	}
-
-	//通常性的全显示
-	chrome.pageAction.show(tabId);
 };
 
-//开启ID
-function Set_Link(tabId){
-	//设置开启icon
-	chrome.pageAction.setIcon({tabId: tabId, path: "./img/link_38.png"});
+//显示icon图标
+function show_icon(tabID){
+	//通常性的全显示
+	chrome.pageAction.show(tabId);
 }
 
-//关闭Icon
-function Set_Broken(tabId){
-	//设置关闭icon
-	chrome.pageAction.setIcon({tabId: tabId, path: "./img/broken_38.png"});
-}
-
-//点击事件-它肯定是存在才有
-chrome.pageAction.onClicked.addListener(function(tab){
-	//注销玩意
-	Set_Broken(tab.id);//破灭
-	//jQuery注入移除
-	chrome.tabs.sendMessage(tab.id,{work: "remove"}); //送入命令
-})
-
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+chrome.tabs.onUpdated.addListener(chkfixurl);
